7867cbb3e4acd8a1b723bcb9b2454df4bb079e46
vladim <vladim@cobuilder.no>
2023-08-31 12:14:01 +0300

    update and delete commands

---
+++ src/Core/PeopleCatalogue.Application/Features/Person/Commands/DeletePerson/DeletePersonCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace PeopleCatalogue.Application.Features.Person.Commands.DeletePerson
+{
+    public class DeletePersonCommand : IRequest<Unit>
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ src/Core/PeopleCatalogue.Application/Features/Person/Commands/UpdatePerson/UpdatePersonCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace PeopleCatalogue.Application.Features.Person.Commands.UpdatePerson
+{
+    public class UpdatePersonCommand : IRequest<Unit>
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string Address { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Iban { get; set; }
+    }
+}
