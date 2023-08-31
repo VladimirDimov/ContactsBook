1fa3c6cd9c6713ae8ffd16e435c4853d3cec097c
vladim <vladim@cobuilder.no>
2023-08-31 18:29:21 +0300

    add initial migration

--- src/Infrastructure/PeopleCatalogue.Persistence/Configurations/ContactConfiguration.cs
+++ src/Infrastructure/PeopleCatalogue.Persistence/Configurations/ContactConfiguration.cs
@@ -27,10 +27,57 @@ namespace ContactsBook.Persistence.Configurations
                 .HasMaxLength(200);
 
             builder.Property(x => x.Iban)
-                .HasMaxLength(200);
+                .HasMaxLength(34);
 
             builder.Property(x => x.CreatedOn)
                 .IsRequired();
+
+            builder.HasData(
+                new Contact
+                {
+                    Id = 1,
+                    FirstName = "Adam",
+                    LastName = "Baum",
+                    Address = "Sofia, Iztok",
+                    Iban = "BG18RZBB91550123456789",
+                    CreatedOn= DateTime.UtcNow,
+                    DateOfBirth = new DateTime(1980, 5, 6),
+                    PhoneNumber = "555-00001"
+                },
+                new Contact
+                {
+                    Id = 2,
+                    FirstName = "Bob",
+                    LastName = "Apple",
+                    Address = "Plovdiv, Izgrev",
+                    Iban = "BG18RZBB91550123456789",
+                    CreatedOn = DateTime.UtcNow,
+                    DateOfBirth = new DateTime(1984, 2, 15),
+                    PhoneNumber = "555-00002"
+                },
+                new Contact
+                {
+                    Id = 3,
+                    FirstName = "Dusty",
+                    LastName = "Carr",
+                    Address = "Sofia, Geo Milev 123",
+                    Iban = "BG18RZBB91550123456789",
+                    CreatedOn = DateTime.UtcNow,
+                    DateOfBirth = new DateTime(1991, 5, 6),
+                    PhoneNumber = "777-88881"
+                },
+                new Contact
+                {
+                    Id = 4,
+                    FirstName = "Chris",
+                    LastName = "Cross",
+                    Address = "Sofia, Iztok",
+                    Iban = "BG18RZBB91550123456789",
+                    CreatedOn = DateTime.UtcNow,
+                    DateOfBirth = new DateTime(1972, 3, 22),
+                    PhoneNumber = "777-51516"
+                }
+            );
         }
     }
 }
---
+++ src/Infrastructure/PeopleCatalogue.Persistence/Migrations/20230831152844_initial.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ContactsBook.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contacts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    DateOfBirth = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Address = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: true),
+                    Iban = table.Column<string>(type: "nvarchar(34)", maxLength: 34, nullable: true),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedOn = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contacts", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Contacts",
+                columns: new[] { "Id", "Address", "CreatedOn", "DateOfBirth", "FirstName", "Iban", "LastName", "PhoneNumber", "UpdatedOn" },
+                values: new object[,]
+                {
+                    { 1, "Sofia, Iztok", new DateTime(2023, 8, 31, 15, 28, 44, 198, DateTimeKind.Utc).AddTicks(1635), new DateTime(1980, 5, 6, 0, 0, 0, 0, DateTimeKind.Unspecified), "Adam", "BG18RZBB91550123456789", "Baum", "555-00001", null },
+                    { 2, "Plovdiv, Izgrev", new DateTime(2023, 8, 31, 15, 28, 44, 198, DateTimeKind.Utc).AddTicks(1643), new DateTime(1984, 2, 15, 0, 0, 0, 0, DateTimeKind.Unspecified), "Bob", "BG18RZBB91550123456789", "Apple", "555-00002", null },
+                    { 3, "Sofia, Geo Milev 123", new DateTime(2023, 8, 31, 15, 28, 44, 198, DateTimeKind.Utc).AddTicks(1645), new DateTime(1991, 5, 6, 0, 0, 0, 0, DateTimeKind.Unspecified), "Dusty", "BG18RZBB91550123456789", "Carr", "777-88881", null },
+                    { 4, "Sofia, Iztok", new DateTime(2023, 8, 31, 15, 28, 44, 198, DateTimeKind.Utc).AddTicks(1646), new DateTime(1972, 3, 22, 0, 0, 0, 0, DateTimeKind.Unspecified), "Chris", "BG18RZBB91550123456789", "Cross", "777-51516", null }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contacts");
+        }
+    }
+}
