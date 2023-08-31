2977165664e7aac319dfc7b5aaf7cca2fc66e1b3
vladim <vladim@cobuilder.no>
2023-08-31 09:09:12 +0300

    create person handler

---
+++ src/Core/PeopleCatalogue.Application/Features/Person/Commands/CreatePerson/CreatePersonCommand.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+
+namespace PeopleCatalogue.Application.Features.Person.Commands.CreatePerson
+{
+    public class CreatePersonCommand : IRequest<int>
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string Address { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Iban { get; set; }
+    }
+}
---
+++ src/Core/PeopleCatalogue.Application/Features/Person/Commands/CreatePerson/CreatePersonCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using MediatR;
+using PeopleCatalogue.Application.Contracts.Persistence;
+
+namespace PeopleCatalogue.Application.Features.Person.Commands.CreatePerson
+{
+    public class CreatePersonCommandHandler : IRequestHandler<CreatePersonCommand, int>
+    {
+        private readonly IMapper _mapper;
+        private readonly IPersonRepository _personRepository;
+
+        public CreatePersonCommandHandler(
+            IMapper mapper,
+            IPersonRepository personRepository)
+        {
+            _mapper = mapper;
+            _personRepository = personRepository;
+        }
+
+        public async Task<int> Handle(CreatePersonCommand request, CancellationToken cancellationToken)
+        {
+            var person = _mapper.Map<Domain.Person>(request);
+
+            var createdPerson = await _personRepository.CreateAsync(person);
+
+            return createdPerson.Id;
+        }
+    }
+}
