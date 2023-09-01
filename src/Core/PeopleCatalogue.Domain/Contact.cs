8ce04407cca8e24db2e680fbec66be4bdc994d2a
vladim <vladim@cobuilder.no>
2023-09-01 08:10:50 +0300

    rich domain model

--- src/Core/PeopleCatalogue.Domain/Contact.cs
+++ src/Core/PeopleCatalogue.Domain/Contact.cs
@@ -2,16 +2,32 @@
 {
     public class Contact : BaseEntity
     {
-        public string FirstName { get; set; } = string.Empty;
+        public Contact(
+            string firstName,
+            string lastName,
+            DateTime? dateOfBirth,
+            string? address,
+            string? phoneNumber,
+            string? iban)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            DateOfBirth = dateOfBirth;
+            Address = address;
+            PhoneNumber = phoneNumber;
+            Iban = iban;
+        }
 
-        public string LastName { get; set; } = string.Empty;
+        public string FirstName { get; private set; } = string.Empty;
 
-        public DateTime? DateOfBirth { get; set; }
+        public string LastName { get; private set; } = string.Empty;
 
-        public string? Address { get; set; }
+        public DateTime? DateOfBirth { get; private set; }
 
-        public string? PhoneNumber { get; set; }
+        public string? Address { get; private set; }
 
-        public string? Iban { get; set; }
+        public string? PhoneNumber { get; private set; }
+
+        public string? Iban { get; private set; }
     }
 }
