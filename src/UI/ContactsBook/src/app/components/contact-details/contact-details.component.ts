b92c6712b6ac654b07e2cbeac9efbc0763fd00c6
vladim <vladim@cobuilder.no>
2023-09-03 09:57:43 +0300

    contact details form

--- src/UI/ContactsBook/src/app/components/contact-details/contact-details.component.ts
+++ src/UI/ContactsBook/src/app/components/contact-details/contact-details.component.ts
@@ -1,11 +1,12 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-details',
   templateUrl: './contact-details.component.html',
   styleUrls: ['./contact-details.component.scss'],
 })
-export class ContactDetailsComponent {
+export class ContactDetailsComponent implements OnInit {
   private _contactId: number = 0;
   @Input()
   public get contactId(): number {
@@ -17,4 +18,27 @@ export class ContactDetailsComponent {
   }
 
   @Output() contactIdChange = new EventEmitter<number>();
+
+  contactUpdateForm: FormGroup<any> = new FormGroup({});
+
+  ngOnInit(): void {
+    this.contactUpdateForm = new FormGroup({
+      firstName: new FormControl(null, [
+        Validators.required,
+        Validators.maxLength(30),
+      ]),
+      lastName: new FormControl(null, [
+        Validators.required,
+        Validators.maxLength(30),
+      ]),
+      dateOfBirth: new FormControl(null),
+      phoneNumber: new FormControl(null, [
+        Validators.required,
+        Validators.maxLength(20),
+      ]),
+      iban: new FormControl(null, [Validators.maxLength(34)]),
+    });
+  }
+
+  onSubmit() {}
 }
