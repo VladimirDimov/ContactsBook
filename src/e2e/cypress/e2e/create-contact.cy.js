778494ecf9048f65538e5479d1e4200502ad91aa
vladim <vladim@cobuilder.no>
2023-09-04 08:37:59 +0300

    create contact e2e test

--- src/e2e/cypress/e2e/create-contact.cy.js
+++ src/e2e/cypress/e2e/create-contact.cy.js
@@ -1,3 +1,4 @@
+import { expect } from "chai";
 import { aboutUrl, baseUrl, homeUrl } from "./constants";
 
 describe("add contact dialog opens correctly", () => {
@@ -25,6 +26,25 @@ describe("user can create contact", () => {
     // navigate to /about
     cy.get(`p-button[label="Add Contact"`).click();
 
-    cy.get("#firstName");
+    cy.get("#firstName").type("Claude");
+    cy.get("#lastName").type("Monet");
+    cy.get("#dateOfBirth").type("11/14/1840");
+    cy.get("#phoneNumber").type("111-222222");
+    cy.get("#iban").type("NL39ABNA9403840137");
+    cy.get(`p-button[label="Submit"`).click();
+    cy.get("tr")
+      .last()
+      .within(() => {
+        cy.get("td").then((td) => {
+          cy.get(td[0]).contains("Claude");
+          cy.get(td[1]).contains("Monet");
+          cy.get(td[2]).contains("14-11-1840");
+          cy.get(td[3]).contains("111-222222");
+          cy.get(td[4]).contains("NL39ABNA9403840137");
+
+          // clean after assert
+          cy.get('p-button[icon="pi pi-trash"]').click();
+        });
+      });
   });
 });
